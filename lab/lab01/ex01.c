92650ba27d708d6ee64b1ea1b2df63b9bd1c77c3
Berkay Zaim <88087972+berkayzaimdev@users.noreply.github.com>
2024-03-14 07:22:36 +0300

    Update ex01.c

--- lab/lab01/ex01.c
+++ lab/lab01/ex01.c
@@ -1,36 +1,25 @@
 #include <stdio.h>
-#include <stdint.h>
-#include <inttypes.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
 
-
-
-void DumpHex(void* pointer, int byteLen){ 
-  int i;
-  printf("\nThe %d bytes starting at %p are: ",byteLen,pointer); 
-  for(i=0;i<byteLen;i++)
-  { 
-    printf("%02x ",*((unsigned char *)pointer+i));
-  }
+int main() {
+   int var = 10;
+   printf("Parent process: var = %d, PID = %d\n", var, getpid());
+   fork();
+   printf("After first fork: var = %d, PID = %d\n", var, getpid());
+   fork();
+   printf("After second fork: var = %d, PID = %d\n", var, getpid());
+   fork();
+   printf("After third fork: var = %d, PID = %d\n", var, getpid());
+   return 0;
 }
 
-int main(int argc, char **argv){
-
-  char charVal = '0';
-  int32_t intVal = 1;
-  float floatVal = 1.0;
-  double doubleVal = 1.0;
-
-  typedef struct {
-  char charVal;
-  int32_t intVal;
-  float floatVal;                                                 
-  double doubleVal;       
-  } Ex2Struct; Ex2Struct structVal = { '0', 1, 1.0, 1.0 };  
-  
-  DumpHex(&charVal, sizeof(char));            
-  DumpHex(&intVal, sizeof(int32_t));          
-  DumpHex(&floatVal, sizeof(float));       
-  DumpHex(&doubleVal, sizeof(double));
-  DumpHex(&structVal, sizeof(structVal));
-  return 0;
-}
+// Adım adım gidecek olursak; ilk fork işleminden önce Parent process çıktısı yazılır.
+// İlk fork işlemi, proccess'imizde yeni bir dal oluşturur ve process 9.satırdan devam eder.
+// Bu dallar, first fork sonucunu iki kere yazdırır. Parent ve Child sırası farklı olabilir.
+// İkinci fork işlemi, devam etmekte olan iki process'i ayrı ayrı ikiye ayırır.
+// Bu ayrılan yeni dallar, 12. satırdan devam eder. Yani 4 ayrı process, 4 kez second fork sonucunu yazdırır.
+// 13. satırda üçüncü fork işlemi uygulanır ve 4'e ayrılmış olan process'lerin her biri tekrar 2'ye ayrılır.
+// Sonuç olarak third fork 4*2 = 8 kez yazdırılır.
+// Çıktı sıraları, processlerin sonuçlanma durumuna göre farklılık gösterebilir.
